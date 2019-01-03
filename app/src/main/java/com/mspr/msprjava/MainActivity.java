3a83e893bc028388924025781d3445f081c93e29
FlorianCap <sevrain.florian@gmail.com>
2019-01-03 22:11:53 +0100

    mise en place cg + ecran accueil

--- app/src/main/java/com/mspr/msprjava/MainActivity.java
+++ app/src/main/java/com/mspr/msprjava/MainActivity.java
@@ -1,14 +1,19 @@
 package com.mspr.msprjava;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
-        
+        setContentView( R.layout.activity_accueil );
+
+        Typeface tf =  Typeface.createFromAsset( getAssets(), "fonts/Medium.ttf" );
+        Button signIn = (Button) findViewById( R.id.signIn );
+        signIn.setTypeface( tf );
     }
 }
