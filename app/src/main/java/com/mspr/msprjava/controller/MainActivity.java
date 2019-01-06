abb35118beb002672e30a9a7c089e084accf2196
FlorianCap <sevrain.florian@gmail.com>
2019-01-06 14:06:16 +0100

    insert provider file + manifest

--- app/src/main/java/com/mspr/msprjava/controller/MainActivity.java
+++ app/src/main/java/com/mspr/msprjava/controller/MainActivity.java
@@ -1,25 +1,29 @@
 package com.mspr.msprjava.controller;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.FirebaseApp;
 import com.mspr.msprjava.R;
+import com.mspr.msprjava.model.CameraActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_accueil );
-
+        signIn = (Button)findViewById(R.id.signIn);
         Typeface tf =  Typeface.createFromAsset( getAssets(), "fonts/Roboto-Medium.ttf" );
-/*        Button signIn = (Button) findViewById( R.id.signIn );
-        signIn.setTypeface( tf );*/
+        signIn.setTypeface( tf );
+        //createOnClickBtnSignIn();
         FirebaseApp.initializeApp(this);
+        createOnClickBtnSignIn();
     }
 
     protected void onStart(){
@@ -42,5 +46,17 @@ public class MainActivity extends AppCompatActivity {
         Log.v( "onStop", "Good" );
     }
 
+    private void createOnClickBtnSignIn(){
+        signIn = (Button) findViewById( R.id.signIn );
+        signIn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                takePicture();
+            }
+        });
+    }
 
+    private void takePicture() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
 }
